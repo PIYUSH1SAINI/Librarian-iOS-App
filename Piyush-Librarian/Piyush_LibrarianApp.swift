b316d038e39429a973439270b6caefa1e418e9d6
PIYUSH1SAINI <piyushsaini20001@gmail.com>
2024-05-31 14:21:00 +0530

    Initial Commit

---
+++ Piyush-Librarian/Piyush_LibrarianApp.swift
@@ -0,0 +1,17 @@
+//
+//  Piyush_LibrarianApp.swift
+//  Piyush-Librarian
+//
+//  Created by Piyush saini on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Piyush_LibrarianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
